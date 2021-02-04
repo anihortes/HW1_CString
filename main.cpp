22c7cc008a463bfd577ddb0e5e3632f7a324c956
Adam Owen <ahowenjr@alaska.edu>
2021-02-03 17:28:20 -0900

    added function that copies c-string into memory

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 
+char* strup(const char* s){
+    char* ptr = new char;
+    auto newPtr = ptr;
+    while(*s != '\0'){
+        *ptr = *s;
+        ptr++;
+        s++;
+    }
+    *ptr = '\0';
+    return newPtr;
+}
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
+
