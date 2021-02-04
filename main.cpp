84f3c1aa66ab6ade523a87421a49b979de45ce49
Adam Owen <ahowenjr@alaska.edu>
2021-02-03 17:39:19 -0900

    started function findx

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+using std::cout;
+using std::endl;
 #include <string>
 using std::string;
 
@@ -14,11 +16,19 @@ char* strdup(const char* s){
     return newPtr;
 }
 
+char* findx(const char* s, const char* x){
+
+}
+
 int main() {
     string temp = "this string";
     const char* cstr = temp.c_str();
     strdup(cstr);
-    std::cout << cstr << std::endl;
+    cout << cstr << endl;
+
+    string xString = "this word is lame right?";
+    string sString = "i never said this word was lame";
+    cout << xString << sString << endl;
     return 0;
 }
 
